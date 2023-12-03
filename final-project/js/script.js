1716da6be882a9d4ad862048ebe0f944166b8182
kavyaiyerr <95099962+kavyaiyerr@users.noreply.github.com>
2023-12-03 17:49:29 -0500

    Youtube API call updates

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,6 +1,5 @@
 //function to display visibility of sections
 function hideSearch() {
-    event.preventDefault()
     let section1 = document.querySelector('.search'); // Add a dot for the class selector
     let section2 = document.querySelector('.results'); // Add a dot for the class selector
     section1.style.display = "none";
@@ -14,57 +13,88 @@ function hideResults() {
     section2.style.display = "none";
 }
 
-//CODE TO DISPLAY SEARCH RESULTS
-
-
-// function to grab form field answers from home page
+//CODE TO DISPLAY SEARCH RESULTS 
+//(reference: https://developers.google.com/youtube/v3/docs/search/list?apix_params=%7B%22part%22%3A%5B%22snippet%22%5D%2C%22maxResults%22%3A48%2C%22q%22%3A%22kiss%20you%20%2B%20one%20direction%20%2B%20cover%22%2C%22type%22%3A%5B%22video%22%5D%7D)
 
 //functions to make API call
 
-function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
+//function to load the YouTube API client with the API key
 function loadClient() {
-    gapi.client.setApiKey("YOUR_API_KEY");
+    gapi.client.setApiKey("AIzaSyCGr4-POpx4JwHQp1u62GkZxgVDL8vplNA");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
-  }
+}
 
+//function to execute the YouTube API search
 function execute() {
-    //authorize & load client
-    authenticate().then(loadClient);
+    //load the YouTube API client with the API key
+    return loadClient()
+        .then(function() {
+            //grab form field entries
+            let songTitle = document.getElementById('song-title').value;
+            let artist = document.getElementById('song-artist').value;
 
-    //grab form field entries
-    let songTitle = document.getElementById('song-title').value;
-    let artist = document.getElementById('song-artist').value;
-
-    //execute API call
-    return gapi.client.youtube.search.list({
-      "part": [
-        "snippet"
-      ],
-      "maxResults": 48,
-      "q": songTitle + artist + "cover",
-      "type": [
-        "video"
-      ]
-    })
+            //execute the API call
+            return gapi.client.youtube.search.list({
+                "part": ["snippet"],
+                "maxResults": 48,
+                "q": songTitle + artist + "cover",
+                "type": ["video"]
+            });
+        })
         .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
-  });
+            //handle the results here (response.result has the parsed body).
+            console.log("Response", response);
+            return response;
+        })
+        .catch(function(err) {
+            console.error("Execute error", err);
+        });
+}
 
 //function to populate result cards
+function generateCards() {
+    execute()
+        .then(function(response) {
+            let searchResults = response.result.items;
 
+            const allCards = document.querySelectorAll('.card');
+
+            for (let i = 0; i < searchResults.length && i < allCards.length; i++) {
+                const result = searchResults[i];
+                const card = allCards[i];
+
+                //update thumbnail
+                const thumbnailUrl = result.snippet.thumbnails.medium.url;
+                card.querySelector('.image').src = thumbnailUrl;
+                card.querySelector('.image').alt = `Song ${i + 1}`;
+
+                //update title and channel title
+                const title = result.snippet.title;
+                const channelTitle = result.snippet.channelTitle;
+                card.querySelector('.title').textContent = title;
+                card.querySelector('.artist').textContent = channelTitle;
+
+                //update audio source
+                const audioSource = `https://www.youtube.com/watch?v=${result.id.videoId}`;
+                card.querySelector('.link').src = audioSource;
+            }
+        });
+}
+
+//get the form element
+const form = document.querySelector('form');
+
+//event listener for card generation
+form.addEventListener('submit', function (event) {
+  //prevent the default form submission
+  event.preventDefault();
+
+  //hide search part & display results part
+  hideSearch();
+  generateCards();
+});
 
 //CODE FOR LIKING SONGS
 class Song {
