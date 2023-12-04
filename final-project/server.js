86b9ae467702fd3bfd67bd5cb007e4695a637f00
kavyaiyerr <95099962+kavyaiyerr@users.noreply.github.com>
2023-12-04 16:41:45 -0500

    card populating function achieved

--- final-project/server.js
+++ final-project/server.js
@@ -1,3 +1,4 @@
+
 function fetchSongs() {
     var URL="https://www.googleapis.com/youtube/v3/search";
     var key= 'AIzaSyCGr4-POpx4JwHQp1u62GkZxgVDL8vplNA';
@@ -8,48 +9,74 @@ function fetchSongs() {
     let searchquery = songTitle + 'by'+ artist + 'covers';
 
     var options = {
+        part: 'snippet',
         key: key,
         maxResults: 12,
         q: searchquery,
-        part: ["snippet"],
+        type: 'video',
     }
 
-    loadVids();
+    // Return a promise that resolves with the data
+    return new Promise(function(resolve, reject) {
+        $.getJSON(URL, options, function(data) {
+            resolve(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            reject(errorThrown);
+        });
+    });
+
+    /*return loadVids();
 
     function loadVids(){
         $.getJSON(URL, options, function(data){
             console.log(data);
         })
-    }
+        return $.getJSON(URL, options);
+    } */
 
 }
 
 function Generate() {
-    let section1 = document.querySelector('.search'); // Add a dot for the class selector
-    let section2 = document.querySelector('.results'); // Add a dot for the class selector
+    let section1 = document.querySelector('.search'); 
+    let section2 = document.querySelector('.results'); 
     section1.style.display = "none";
     section2.style.display = "block";
-    console.log("generating");
-    // let songTitle = document.getElementById('song-title').value;
-    // let artist = document.getElementById('song-artist').value;
-    // let searchquery = songTitle + 'by'+ artist + 'covers';
-    // var opts = {
-    //     maxResults: 12,
-    //     key: 'AIzaSyCGr4-POpx4JwHQp1u62GkZxgVDL8vplNA'
-    //   };
-      
-    //   search(searchquery, opts, function(err, results) {
-    //     if(err) return console.log(err);
-      
-    //     console.dir(results);
-    //   });
-    fetchSongs();
+    
+    // Call fetchSongs and handle the resolved data
+    fetchSongs().then(function(data) {
+        searchResults = data.items;
+        //populate cards
+        let allCards = document.querySelectorAll('.card');
+        console.log(allCards);
+
+        for (let i = 0; i < searchResults.length && i < allCards.length; i++) {
+            let result = searchResults[i];
+            let card = allCards[i];
+            console.log(card);
+
+            //update thumbnail
+            let thumbnailUrl = result.snippet.thumbnails.medium.url;
+            card.querySelector('.image').src = thumbnailUrl;
+            card.querySelector('.image').alt = `Song ${i + 1}`;
+
+            //update title and channel title
+            let title = result.snippet.title;
+            let channelTitle = result.snippet.channelTitle;
+            card.querySelector('.title').textContent = title;
+            card.querySelector('.artist').textContent = channelTitle;
+
+            //update audio source
+            //const audioSource = `https://www.youtube.com/watch?v=${result.id.videoId}`;
+            //card.querySelector('.link').src = audioSource;
+        }
+
+    })
 
 }
 
 function hideResults() {
-    let section1 = document.querySelector('.search'); // Add a dot for the class selector
-    let section2 = document.querySelector('.results'); // Add a dot for the class selector
+    let section1 = document.querySelector('.search'); 
+    let section2 = document.querySelector('.results'); 
     section1.style.display = "block";
     section2.style.display = "none";
 }
